ebf7a91b5dc6a1489eca7c9bb439ccced82ac035
Somphong Phuangmalai <somphong.ph@gmail.com>
2020-08-15 20:24:33 +0700

    redis cach

--- Controllers/PlacesController.cs
+++ Controllers/PlacesController.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using netcore_google_map.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
 
 // using System.IO.TextWriter;
 
@@ -34,22 +35,36 @@ namespace netcore_google_map.Controllers
         public async Task<ActionResult<List<Place>>> Get([FromQuery]string keyword)
         {
             List<Place> place = null;
+
+            // Get Redis cache
             var inCache = _distributedCache.GetString(keyword);
             if (!string.IsNullOrEmpty(inCache)) {
-                return NotFound();
-            } else {
-                var googleMapApiUrl = _configuration["GoogleMapApi:Url"];
-                var googleMapApiKey = _configuration["GoogleMapApi:Key"];
+                var getData = await _distributedCache.GetAsync(keyword);
+                var bytesAsString = Encoding.UTF8.GetString(getData);
+                place = JsonConvert.DeserializeObject<List<Place>>(bytesAsString);
 
-                string url = googleMapApiUrl + "/place/textsearch/json?query=restaurants+in+" + keyword + "&key=" + googleMapApiKey;
-                var client = new HttpClient();
-                var result = await client.GetStringAsync(String.Format(url));
+                return Ok(place);
+            }   
+           
+            // Get Google map api
+            var googleMapApiUrl = _configuration["GoogleMapApi:Url"];
+            var googleMapApiKey = _configuration["GoogleMapApi:Key"];            
+
+            string url = googleMapApiUrl + "/place/textsearch/json?query=restaurants+in+" + keyword + "&key=" + googleMapApiKey;
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(String.Format(url));        
+
+            var jsonObject = JsonConvert.DeserializeObject<RootObject>(result);
+            place = jsonObject.results;
             
-
-                var jsonObject = JsonConvert.DeserializeObject<RootObject>(result);
-
-                place = jsonObject.results; 
-            }
+            // Set Redis cache
+            var expirationMinutes = Convert.ToDouble(_configuration["Redis:ExpirationMinutes"]);
+            string serializeObject = JsonConvert.SerializeObject(place);
+            byte[] data = Encoding.UTF8.GetBytes(serializeObject);
+            await _distributedCache.SetAsync(keyword, data, new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(expirationMinutes)
+            });
 
             return Ok(place);
         }
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using StackExchange.Redis;
 
 namespace netcore_google_map
 {
@@ -50,15 +42,6 @@ namespace netcore_google_map
                 options.Configuration = Configuration["Redis:ConnectionString"];
                 options.InstanceName = Configuration["Redis:InstanceName"];                
             });
-
-            // IConnectionMultiplexer redis = ConnectionMultiplexer.Connect("10.0.75.1");
-            // services.AddScoped(s => redis.GetDatabase());
-
-            // services.AddStackExchangeRedisCache(options =>
-            // {
-            //     options.Configuration = "localhost";
-            //     options.InstanceName = "SampleInstance";
-            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
